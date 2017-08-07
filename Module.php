a0b2bf9200d6afbb70efa0899912ad6ee6eb976f
nadine-shi <nadine@afterlogic.com>
2017-08-07 15:18:33 +0300

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AGPL-3.0 or AfterLogic Software License
+ *
+ * This code is licensed under AGPLv3 license or AfterLogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Aurora\Modules\OAuthIntegratorMobileWebclient;
+
+/**
+ * @package Modules
+ */
+class Module extends \Aurora\System\Module\AbstractWebclientModule
+{
+	/***** private functions *****/
+	/**
+	 * Initializes OAuthIntegratorMobileWebclient Module.
+	 * 
+	 * @ignore
+	 */
+	public function init() 
+	{
+		parent::init();
+		
+		$this->AddEntries(array(
+				'mobile-oauth' => 'EntryMobileOAuth',
+			)
+		);
+	}
+	/***** private functions *****/
+	
+	/***** public functions *****/
+	/**
+	 * @ignore
+	 * @return string
+	 */
+	public function EntryMobileOAuth()
+	{
+		$sResult = \file_get_contents($this->GetPath().'/templates/MobileOAuth.html');
+		$oOAuthModuleDecorator = \Aurora\Modules\OAuthIntegratorWebclient\Module::Decorator();
+		$aServices = $oOAuthModuleDecorator->GetServices();
+		$sResult = strtr($sResult, array(
+			'{{OAUTHINTEGRATORWEBCLIENT/LABEL_ES_SETTINGS_TAB}}' => $oOAuthModuleDecorator->i18N('LABEL_ES_SETTINGS_TAB'),
+			'{{OAUTHINTEGRATORWEBCLIENT/LABEL_SIGN_IN}}' => $oOAuthModuleDecorator->i18N('LABEL_SIGN_IN'),
+			'{{ServicesArray}}' => json_encode($aServices),
+		));
+			
+		return $sResult;
+	}
+	/***** public functions *****/
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# aurora-module-mobile-webclient-oauth-integrator
+# Aurora oAuth Integrator mobile web client module
 Provides oAuth login buttons for mobile client
+
+# License
+This module is licensed under AGPLv3 license if free version of the product is used or AfterLogic Software License if commercial version of the product was purchased.
